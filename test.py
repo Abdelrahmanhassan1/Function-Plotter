f1783b688270c432e28255669587595baa4769a8
abdelrahmanhassan1 <abdelrahmanwork1@gmail.com>
2022-05-06 16:20:50 +0200

    reading equation snippet

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import sympy
 # function to test:
 # x^2 + 5
 
-function = "5/x"
+function = "5*x^2 - x^3"
 
 # first check
 # check if all the equation has numbers, operators, and x nothimg more
@@ -22,13 +22,14 @@ def equation_validation_check(equation):
 
 
 added_expressions = []
-subtracted_expressions = []
+
 new_expression = 1
 x = sympy.symbols("x")
 index = 0
 
 while True:
     if index == len(function):
+        added_expressions.append(new_expression)
         break
     char = function[index]
     if char == "x":
@@ -56,20 +57,35 @@ while True:
 
     elif char.isdigit():
         new_expression *= int(char)
+    elif char == "+":
+        if index != 0:
+            added_expressions.append(new_expression)
+        new_expression = 1
+
+    elif char == "-":
+        if index != 0:
+            added_expressions.append(new_expression)
+        new_expression = -1
+
     index += 1
 
 
 print(str(new_expression))
+full_expression = 0
+for expression in added_expressions:
+    full_expression += expression
+
 x_axis = []
 y_axis = []
 for i in range(1, 11):
-    new_y = new_expression.subs(x, i)
+    new_y = full_expression.subs(x, i)
     if new_y == sympy.zoo:
-        print("asdnasnd")
         break
     x_axis.append(i)
     y_axis.append(new_y)
 
+print(full_expression)
+print(added_expressions)
 print(x_axis)
 print(y_axis)
 
